f3be8effaae07fabcd4876adb8f248fccd4637a5
olivia <larsonog@hendrix.edu>
2021-10-11 22:57:17 -0500

    ideally added in the Razer pages stuff

---
+++ csci340lab7/Models/avocado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace csci340lab7.avocado
+{
+    public class avocado
+    {
+        public int ID { get; set; }
+        public string Type { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Discovery { get; set; }
+        public string bigorsmall { get; set; }
+        public decimal Price { get; set; }
+    }
+}
